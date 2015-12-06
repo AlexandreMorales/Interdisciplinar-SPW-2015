50ac92c6a2db5f15f74c18bc3ee51b6fc211e67a
Alexandre Morales <xandemorales@gmail.com>
2015-12-06 02:23:26 -0200

    criado getlayout

---
+++ PetShop_Cliente/WebContent/js/Layout.js
@@ -0,0 +1,21 @@
+function getLayout(fDone){
+	window.onload = function() {						
+		var container = document.documentElement.innerHTML;
+		window.rooturl = "/PetShop_Cliente/";
+		AjaxPuro({
+			_path : "view/Home/home.html",
+			_done : function(data) {
+				document.documentElement.innerHTML = data;
+				getElement("#container").innerHTML = container;
+				gerenciaLogin();
+				if(fDone)
+					fDone();
+			},
+			_error : function(request, textStatus, error) {
+				var err = textStatus + ", " + error;
+				console.log("Request Failed: " + err);
+			}
+		});
+		window.rooturl = "/PetShop_Servidor/";
+	};
+};
